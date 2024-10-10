1616c7c6b068d22ec80a356d7df6ef19f1de2180
DESKTOP-6I6OMP3\123 <kanaeva.d.08@mail.ru>
2024-10-10 22:11:00 +0300

    lab3

--- labs/settings.py
+++ labs/settings.py
@@ -26,7 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+# DRF
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
