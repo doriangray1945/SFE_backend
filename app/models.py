1e7ef84495eebdc729ce17c626150d527be2ded2
DESKTOP-6I6OMP3\123 <kanaeva.d.08@mail.ru>
2024-10-16 22:55:53 +0300

    lab2 changes

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 from datetime import datetime, time
 
 class Cities(models.Model):
-    STATUS_CHOICES = (
+    STATUS_CHOICES = [
         (1, 'Действует'),
         (2, 'Удалена')
-    )
+    ]
     city_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, blank=False, null=False)
     population = models.CharField(max_length=20, blank=False, null=False)
@@ -22,20 +22,25 @@ class Cities(models.Model):
 
 
 class Applications(models.Model):
-    STATUS_CHOICES = (
-        (1, 'Действует'),
-        (2, 'Удалена')
-    )
+    STATUS_CHOICES = [
+        (1, 'Черновик'),
+        (2, 'Удалена'),
+        (3, 'Сформирована'),
+        (4, 'Завершена'),
+        (5, 'Отклонена'),
+    ]
     app_id = models.AutoField(primary_key=True)
     status = models.IntegerField(choices=STATUS_CHOICES, default=1, blank=False, null=False)
     date_created = models.DateTimeField(blank=False, null=False)
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_applications', blank=True, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_applications', blank=False, null=False)
 
-    submitted = models.DateTimeField(blank=False, null=False)
+    submitted = models.DateTimeField(blank=True, null=True)
     completed = models.DateTimeField(blank=True, null=True)
     moderator = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='moderated_applications',blank=True, null=True)
 
-    vacancy = models.JSONField(default=dict, blank=True)
+    vacancy_name = models.TextField(blank=True, null=True)
+    vacancy_responsibilities = models.TextField(blank=True, null=True)
+    vacancy_requirements = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = True
@@ -49,7 +54,7 @@ class CitiesApplications(models.Model):
     mm_id = models.AutoField(primary_key=True)
     city_id = models.ForeignKey('Cities', models.DO_NOTHING, blank=True, null=True)
     app_id = models.ForeignKey('Applications', models.DO_NOTHING, blank=True, null=True)
-    count = models.IntegerField(blank=True, null=True)
+    count = models.IntegerField(default=1, blank=True, null=True)
 
     class Meta:
         managed = True
