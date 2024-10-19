041f5036d2663913ed8938594a0e4aee5d6bb7f6
DESKTOP-6I6OMP3\123 <kanaeva.d.08@mail.ru>
2024-10-19 23:51:59 +0300

    lab3 changes

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,6 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     path('cities/', views.CitiesList, name='cities_list'),
