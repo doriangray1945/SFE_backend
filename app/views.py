071f69be896d1d80990d47d86a1282c58a473698
DESKTOP-6I6OMP3\123 <kanaeva.d.08@mail.ru>
2025-01-14 02:22:43 +0300

    lab8

--- app/views.py
+++ app/views.py
@@ -894,7 +894,7 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.action in ['create']:
             permission_classes = [AllowAny]
         elif self.action in ['list']:
-            permission_classes = [IsManager | IsAdmin ]
+            permission_classes = [IsManager | IsAdmin]
         else:
             permission_classes = [IsAdmin]
         return [permission() for permission in permission_classes]
